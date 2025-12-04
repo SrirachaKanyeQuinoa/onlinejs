3a2c388965c6a4acd417229c91feeabcf238b32b
SrirachaKanyeQuinoa <60832306+SrirachaKanyeQuinoa@users.noreply.github.com>
2025-12-04 08:54:41 -0800

    Update online_library.js

--- online_library.js
+++ online_library.js
@@ -1,5 +1,5 @@
 window.library = {
-	version: "20251203.1 (online)",
+	version: "20251204.1",
 	assets: [
 		{ type: "photo", thumb: "Family_Photos_3/thumbnails/Family_Photos_3_00002_untitled_0002_0001_thumb.jpg", src: "Family_Photos_3/images_to_process/Family_Photos_3_00002_untitled_0002_0001.jpg", title: "Family at the Park - Scott", date: "2005-07-04", description: "Picnic at Grand Park." },
 		{ type: "photo", thumb: "photos/thumbs/002-birthday.jpg", src: "photos/002-birthday.jpg", title: "Jack's 5th Birthday", date: "2008-11-15", description: "Cake and balloons." },
